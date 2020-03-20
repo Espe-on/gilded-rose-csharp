9ed66bdff86204c807334fde5a30eca8deae4bd2
espe-on <eljoe0@googlemail.com>
2020-03-20 13:59:12 +0000

    Unit test for BackstagePasses

---
+++ GildedRose.Tests/ItemTests/BackstagePassesTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace GildedRose.Tests.ItemTests
+{
+    public class BackstagePassesTests
+    {
+        private List<Item> _testItems;
+        private GildedRose _gildedRose;
+
+        [SetUp]
+        public void Setup()
+        {
+            _testItems = new List<Item>
+            {
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 20,
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 20,
+                }
+            };
+            _gildedRose = new GildedRose(_testItems);
+            const int numberOfDays = 10;
+            for (var i = 0; i < numberOfDays; i++)
+            {
+                _gildedRose.UpdateQuality();
+            }
+        }
+
+        [Test]
+        public void BackstagePassesShouldGainQualityCorrectly()
+        {
+            _testItems[0].Should().BeEquivalentTo(
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 0,
+                    Quality = 45,
+                });
+        }
+        [Test]
+        public void BackstagePassesShouldLoseQualityCorrectly()
+        {
+            _testItems[1].Should().BeEquivalentTo(
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = -5,
+                    Quality = 0,
+                });
+        }
+    }
+}
