e9b36ecbca4577df3f960066e587247e1716ce0a
espe-on <eljoe0@googlemail.com>
2020-03-20 13:58:29 +0000

    Unit test for AgedBrie

---
+++ GildedRose.Tests/ItemTests/AgedBrieTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace GildedRose.Tests.ItemTests
+{
+    public class AgedBrieTests
+    {
+        private List<Item> _testItems;
+        private GildedRose _gildedRose;
+
+        [SetUp]
+        public void Setup()
+        {
+            _testItems = new List<Item>
+            {
+                new Item
+                {
+                    Name = "Aged Brie",
+                    SellIn = 30,
+                    Quality = 5,
+                },
+                new Item
+                {
+                    Name = "Aged Brie",
+                    SellIn = 40,
+                    Quality = 45,
+                }
+            };
+            _gildedRose = new GildedRose(_testItems);
+        }
+
+        [Test]
+        public void AgedBrieQualityShouldBeCorrectAfter10Days()
+        {
+            const int numberOfDays = 10;
+            for (var i = 0; i < numberOfDays; i++)
+            {
+                _gildedRose.UpdateQuality();
+            }
+            _testItems[0].Should().BeEquivalentTo(
+                new Item
+                {
+                    Name = "Aged Brie",
+                    SellIn = 20,
+                    Quality = 15,
+                });
+            _testItems[1].Should().BeEquivalentTo(
+                new Item
+                {
+                    Name = "Aged Brie",
+                    SellIn = 30,
+                    Quality = 50,
+                });
+        }
+    }
+}
